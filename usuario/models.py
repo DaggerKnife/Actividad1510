caed870c605680f29dd886515679fb9b3ffa5ae0
DaggerKnife <joshuaz295@gmail.com>
2020-10-22 00:31:46 -0300

    1ra migracion Usuario

---
+++ usuario/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+
+class Genero(models.Model):
+    detalleGenero = models.CharField(max_length=9,min_length=8)
+
+
+class Perfil(models.Model):
+    nombre = models.CharField(max_length=45,min_length=10)
+    apellido = models.CharField(max_length=45)
+    edad = models.IntegerField()
+    fNacimiento = models.DateField()
+    genero = models.ForeignKey(Genero,on_delete=models.CASCADE)
+    usuario= models.OneToOneField(User,on_delete=models.CASCADE)
+    
