28f4627889ed37859c8bb903d64a69f3b440dc08
DaggerKnife <joshuaz295@gmail.com>
2020-10-22 00:39:17 -0300

    Enlace migracion Usuario Verificada

--- usuario/models.py
+++ usuario/models.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import User
 
 
 class Genero(models.Model):
-    detalleGenero = models.CharField(max_length=9,min_length=8)
+    detalleGenero = models.CharField(max_length=9)
 
 
 class Perfil(models.Model):
-    nombre = models.CharField(max_length=45,min_length=10)
+    nombre = models.CharField(max_length=45)
     apellido = models.CharField(max_length=45)
     edad = models.IntegerField()
     fNacimiento = models.DateField()
     genero = models.ForeignKey(Genero,on_delete=models.CASCADE)
     usuario= models.OneToOneField(User,on_delete=models.CASCADE)
-    
+
